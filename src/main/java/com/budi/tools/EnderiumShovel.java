6315b8007a80f4fc69f1f509158ad453a189228f
budi1200 <alenab22@gmail.com>
2014-06-28 13:28:09 +0200

    Added potion effects to tools

--- src/main/java/com/budi/tools/EnderiumShovel.java
+++ src/main/java/com/budi/tools/EnderiumShovel.java
@@ -5,10 +5,13 @@ import com.budi.core.budimain;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 
 public class EnderiumShovel extends ItemSpade
 {
@@ -16,5 +19,11 @@ public class EnderiumShovel extends ItemSpade
     {
         super(material);
         this.setCreativeTab(budimain.tabrandom);
+    }
+    public boolean hitEntity(ItemStack par1ItemStack, EntityLivingBase par2EntityLivingBase, EntityLivingBase par3EntityLivingBase)
+    {
+        par2EntityLivingBase.addPotionEffect(new PotionEffect(Potion.confusion.id, 100, 3));
+        par2EntityLivingBase.addPotionEffect(new PotionEffect(Potion.blindness.id, 100, 3));
+        return true;
     }
 }
